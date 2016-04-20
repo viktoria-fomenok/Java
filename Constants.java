0e4df2af26c15c029c38bc6e149f773dc870796f
AlekseyKozlov <andreibikerrr@gmail.com>
2016-04-20 22:33:41 +0300

    laba2
    Change-Id: I561200279dda40b6f9b2da652c13018f9559f1bc

---
+++ Constants.java
@@ -0,0 +1,19 @@
+package application;
+
+/**
+ *
+ * @author vikyf_000 Some constant numbers are defined here.
+ *
+ */
+public interface Constants {
+  final static int SCREEN_HEIGHT = 1000;
+  final static int SCREEN_WIDTH = 600;
+  final static int IMAGE_WIDTH = 610;
+  final static int IMAGE_HEIGHT = 1010;
+  final static int SIZE_MENU_BOX = 10;
+  final static int OFFSET = 400;
+  final static int MENU_TRANSLATE_X = 600;
+  final static int MENU_TRANSLATE_Y = 50;
+}
+
+
