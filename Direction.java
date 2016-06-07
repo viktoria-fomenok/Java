2db670b76b4bb20282506abe6f022c77e877b3a4
Viktoria Fomenok <viky.fomenok@gmail.com>
2016-06-07 13:03:21 +0300

    sn

--- Direction.java
+++
@@ -1,5 +0,0 @@
-package enums;
-
-public enum Direction {
-    UP, DOWN, LEFT, RIGHT;
-  }
--- GlobalVariables.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
-
-public class GlobalVariables {
-  public static Stage stage = new Stage();
-  public static ImageView imageView;
-}
